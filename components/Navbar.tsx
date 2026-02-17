ec822d6ceeb2fe5a5e596f7da8d59dad088d171a
ZhalgasKracavshik <zhalgas@gmail.com>
2026-02-17 22:14:11 +0500

    Navbar logo redirects to /home for logged-in users

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ export async function Navbar() {
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container mx-auto flex h-14 items-center px-4 md:px-6">
                 <div className="mr-4 flex">
-                    <Link href="/" className="mr-6 flex items-center space-x-2">
+                    <Link href={user ? "/home" : "/"} className="mr-6 flex items-center space-x-2">
                         <span className="font-extrabold text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-600">
                             ULAGAT
                         </span>
